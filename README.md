f9ee7f6fa7c08647e282d640663abeae9dd620a6
Alexandros Bantis <ambantis@gmail.com>
2013-05-05 17:11:34 -0700

    exercise 1-4

---
+++ README.md
@@ -0,0 +1,5 @@
+K&R - The C Programming Language
+================================
+
+Just interested in brushing up on my C a bit, dusting off
+my copy of K&R and taking a look inside.
---
+++ src/1knr/ex04a_1knr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* Exercise 1-3
+   Modify the temperature conversion program to print a heading
+   above the table.
+*/
+
+int main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;   /* lower limit of temperature table */
+    upper = 300; /* upper limit */
+    step = 20;
+
+    celsius = lower;
+    printf("Celsius -> Fahrenheit temperature conversion\n");
+    while (celsius <= upper) {
+	fahr = (9.0/5) * celsius + 32;
+	printf("%3.0f %6.1f\n", celsius, fahr);
+	celsius += step;
+    }
+}
