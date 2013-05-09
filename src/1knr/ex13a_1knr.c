3383d4138ce07f4eb2085baf4c9a03068d989279
Alexandros Bantis <ambantis@gmail.com>
2013-05-08 23:31:41 -0700

    began exercise 1-13

---
+++ src/1knr/ex13a_1knr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/* Exercise 1-13
+ * 
+ * Write a program to print a histogram of the lengths of words in its input.
+ * It is easy to draw the histogram with the bars horizontal; a vertical
+ * orientation is more challenging.
+ *
+ * Author: Alexandros Bantis
+ * Date: 2013/05/08
+ * Time: 23:03
+ */ 
+
+int main()
+{
+    int wcount[30];
+    int c, i;
+
+    for(i = 0; i < 30; i++) {
+        wcount[i] = 0;
+    }
+
+    while ((c = getchar()) != EOF) {
+        ;
+    }
+}
