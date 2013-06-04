3f2e32ee03c634c5a3ab762048e35c855216277f
Alexandros Bantis <ambantis@gmail.com>
2013-06-03 22:02:58 -0700

    begin exercise 5.3

---
+++ src/5knr/ex03a_5knr.c
@@ -0,0 +1,8 @@
+/**
+ * @file
+ * K&R Exercise 5.3
+ * Write a pointer version of the function strcat that we showed in
+ * Chapter 2: strcat(s,t) copies the string t to the end of s
+ * @date 2013/06/03
+ * @version 1.0a
+ */
