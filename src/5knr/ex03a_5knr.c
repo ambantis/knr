035631790e7fba22a8b7daef58207f8d8b615a40
Alexandros Bantis <ambantis@gmail.com>
2013-06-03 22:50:23 -0700

    exercise 5.3

--- src/5knr/ex03a_5knr.c
+++ src/5knr/ex03a_5knr.c
@@ -6,3 +6,29 @@
  * @date 2013/06/03
  * @version 1.0a
  */
+
+#include <stdio.h>
+
+void strCat(char *s, char *t);
+
+int main()
+{
+    char hello[15] = "Hello ";
+    char world[] = "World!";
+    printf("source: %s\n", hello);
+    printf("extra: %s\n", world);
+    strCat(hello, world);
+    printf("new source: %s\n", hello);
+    return 0;
+}
+
+void strCat(char *s, char *t)
+{
+    while (*s != '\0')
+        s++;
+    while (*t != '\0') {
+        *s = *t;
+        s++;
+        t++;
+    }
+}
